e6bc1ad281d07cc6c321404ae0173f7d7d50c7de
Rachala Ovin <resistlyfe@gmail.com>
2023-05-06 11:27:18 +0530

    chore: changed cli structure to be straightforward

---
+++ src/cli/mod.rs
@@ -0,0 +1,34 @@
+use clap::{arg, command, Args, Parser, Subcommand};
+
+pub mod log;
+
+#[derive(Debug, Clone, Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+
+    /// Path to the directory containing all the environment configurations.
+    #[arg(long, default_value = "/code/configuration")]
+    pub config_dir: String,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+pub enum Commands {
+    Set(SetArgs),
+}
+
+#[derive(Debug, Clone, Args)]
+pub struct SetArgs {
+    /// The name of the environment to set.
+    pub name: String,
+
+    /// Whether root actions should be performed.
+    #[arg(long, default_value = "false")]
+    pub no_root: bool,
+
+    /// Whether to skip installing packages.
+    #[arg(long, short, default_value = "false")]
+    pub no_package_install: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,19 @@ mod environment;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = cli::Args::parse();
-    let env = Environment::from_options(&args.name, &args.config_dir, args.no_root).await?;
+    let args = cli::Cli::parse();
+
+    match args.command {
+        cli::Commands::Set(set_args) => {
+            set_workflow(&args.config_dir, set_args).await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn set_workflow(config_dir: &str, args: cli::SetArgs) -> anyhow::Result<()> {
+    let env = Environment::from_options(&args.name, config_dir, args.no_root).await?;
     let _files = env.read().await?;
 
     let package_install = aur::install_packages(
